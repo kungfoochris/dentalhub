38c4cbea1439eff90e4b0091df6aa3c87df23ec5
Paras Nath <opnchaudhary@gmail.com>
2019-06-26 14:10:22 +0545

    table definition

--- app/src/main/java/com/example/dentalhub/contracts/DBContract.kt
+++ app/src/main/java/com/example/dentalhub/contracts/DBContract.kt
@@ -1,2 +1,26 @@
 package com.example.dentalhub.contracts
 
+import android.provider.BaseColumns
+
+object DBContract{
+
+    class PatientEntry: BaseColumns {
+        companion object {
+            var TABLE_NAME = "patients"
+            var COLUMN_ID = "id"
+            var COLUMN_FIRST_NAME = "first_name"
+            var COLUMN_LAST_NAME = "last_name"
+            var COLUMN_ADDRESS = "address"
+        }
+    }
+
+    class EncounterEntry: BaseColumns{
+        companion object {
+            var TABLE_NAME = "encounters"
+            var COLUMN_ID = "id"
+            var COLUMN_DATE = "date"
+
+        }
+    }
+}
+
