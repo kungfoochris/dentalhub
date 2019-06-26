ebcb8c887c47dafe7010f8a0c8cbab31d53a24ea
Paras Nath <opnchaudhary@gmail.com>
2019-06-26 14:07:20 +0545

    check for user login

--- app/src/main/java/com/example/dentalhub/SplashActivity.kt
+++ app/src/main/java/com/example/dentalhub/SplashActivity.kt
@@ -1,18 +1,29 @@
 package com.example.dentalhub
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 
 class SplashActivity : Activity() {
+    private lateinit var context: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        context = this
 
         Handler().postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
+            val token: String = DentalApp.readFromPreference(context, Constants.PREF_AUTH_TOKEN,"")
+            val email: String = DentalApp.readFromPreference(context, Constants.PREF_AUTH_EMAIL, "")
+            val password: String = DentalApp.readFromPreference(context, Constants.PREF_AUTH_PASSWORD, "")
+            if(token.isEmpty() || email.isEmpty() || password.isEmpty()){
+                startActivity(Intent(this,LoginActivity::class.java))
+            }else{
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+
         }, 3000)
     }
 
