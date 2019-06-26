e92c29a7e0ce3cab8c1187ac8ba02b5ee814eea0
Paras Nath <opnchaudhary@gmail.com>
2019-06-26 14:08:47 +0545

    adapter to display forms

--- app/src/main/java/com/example/dentalhub/adapters/FormPageAdapter.kt
+++ app/src/main/java/com/example/dentalhub/adapters/FormPageAdapter.kt
@@ -1,2 +1,50 @@
 package com.example.dentalhub.adapters
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.dentalhub.fragments.*
+
+class FormPageAdapter(fm: FragmentManager): FragmentPagerAdapter(fm){
+
+    override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> {
+                HistoryFragment()
+            }
+            1 -> {
+                ScreeningFragment()
+            }
+            2 -> {
+                TreatmentFragment()
+            }
+            3 -> {
+                ReferralFragment()
+            }
+            else -> {
+                HistoryFragment()
+            }
+        }
+    }
+
+    override fun getCount() = 4
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position) {
+            0 -> {
+                "History"
+            }
+            1 -> {
+                "Screening"
+            }
+            2-> {
+                "Treatment"
+            }
+            3 -> {
+                "Referral"
+            }
+            else -> {
+                "History"
+            }
+        }
+    }
+}
