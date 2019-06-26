bb73b603cd089be407adf5be71fd10dcf0a79d3f
Paras Nath <opnchaudhary@gmail.com>
2019-06-26 14:06:25 +0545

    EmailValidator util

--- app/src/main/java/com/example/dentalhub/utils/EmailValidator.kt
+++ app/src/main/java/com/example/dentalhub/utils/EmailValidator.kt
@@ -1,2 +1,11 @@
 package com.example.dentalhub.utils
 
+import android.util.Patterns
+
+class EmailValidator {
+    companion object {
+        fun isEmailValid(email: String): Boolean {
+            return Patterns.EMAIL_ADDRESS.toRegex().matches(email)
+        }
+    }
+}
