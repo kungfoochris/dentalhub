e92f564b8b3af93f194a9c571a921a16b316be1b
Paras Nath <opnchaudhary@gmail.com>
2019-06-26 14:06:55 +0545

    DentalApp class

--- app/src/main/java/com/example/dentalhub/DentalApp.kt
+++ app/src/main/java/com/example/dentalhub/DentalApp.kt
@@ -1,2 +1,56 @@
 package com.example.dentalhub
 
+import android.content.Context
+import android.net.ConnectivityManager
+import androidx.multidex.MultiDexApplication
+
+class DentalApp : MultiDexApplication(){
+    companion object Factory {
+        private const val PREF_FILE_NAME = "dentalhub"
+        fun saveToPreference(context: Context, preferenceName: String, preferenceValue: String) {
+            val sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putString(preferenceName, preferenceValue)
+            editor.apply()
+        }
+
+        fun removeFromPreference(context: Context, preferenceName: String) {
+            val prefs = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
+            val editor = prefs.edit()
+            editor.remove(preferenceName)
+            editor.apply()
+
+        }
+        fun readFromPreference(context: Context, preferenceName: String, defaultValue: String): String{
+            val prefs = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
+            return prefs.getString(preferenceName, defaultValue).toString()
+        }
+        fun hasAuthDetails(context: Context): Boolean{
+            val email = readFromPreference(context, Constants.PREF_AUTH_EMAIL,"")
+            val password = readFromPreference(context, Constants.PREF_AUTH_PASSWORD,"")
+            val socialAuth = readFromPreference(context, Constants.PREF_AUTH_SOCIAL, "false")
+            var status = false
+            if(socialAuth == "true"){
+                status = true
+            }
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                status = true
+            }
+            return status
+        }
+
+        fun clearAuthDetails(context: Context){
+            saveToPreference(context, Constants.PREF_AUTH_TOKEN, "")
+            saveToPreference(context, Constants.PREF_AUTH_EMAIL, "")
+            saveToPreference(context, Constants.PREF_AUTH_PASSWORD, "")
+            saveToPreference(context, Constants.PREF_AUTH_SOCIAL, "false")
+        }
+
+        fun isConnectedToWifi(context: Context): Boolean{
+            val connManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+            return mWifi.isConnected
+
+        }
+    }
+}
