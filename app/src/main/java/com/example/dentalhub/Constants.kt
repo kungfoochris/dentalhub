ee600fdc6acc5709915d78448c55653d5ead0522
Paras Nath <opnchaudhary@gmail.com>
2019-06-26 14:05:39 +0545

    constants file

--- app/src/main/java/com/example/dentalhub/Constants.kt
+++ app/src/main/java/com/example/dentalhub/Constants.kt
@@ -1,2 +1,18 @@
 package com.example.dentalhub
 
+class Constants{
+
+    companion object {
+        @JvmStatic val CONTENT_TYPE_TEXT = "text/plain"
+        @JvmStatic val CONTENT_TYPE_IMAGE = "image/*"
+        @JvmStatic val CONTENT_TYPE_VIDEO = "video/*"
+
+        @JvmStatic val PREF_AUTH_TOKEN = "AUTH-TOKEN"
+        @JvmStatic val PREF_AUTH_EMAIL = "AUTH-EMAIL"
+        @JvmStatic val PREF_AUTH_PASSWORD = "AUTH-PASSWORD"
+        @JvmStatic val PREF_AUTH_SOCIAL = "AUTH-SOCIAL"
+    }
+
+
+
+}
