a9b0f1c4c3d51805468bea56a4145bad209909e6
Paras Nath <opnchaudhary@gmail.com>
2019-06-26 16:07:36 +0545

    adding button

--- app/src/main/java/com/example/dentalhub/AddPatientActivity.kt
+++ app/src/main/java/com/example/dentalhub/AddPatientActivity.kt
@@ -1,14 +1,17 @@
 package com.example.dentalhub
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import android.widget.SpinnerAdapter
 import androidx.appcompat.app.AppCompatActivity
 
 class AddPatientActivity : AppCompatActivity(){
 
+    private lateinit var btnAddPatient: Button
     private lateinit var spinnerGender: Spinner
     private lateinit var spinnerMaritalStatus: Spinner
     private lateinit var spinnerEducationLevel: Spinner
@@ -25,17 +28,22 @@ class AddPatientActivity : AppCompatActivity(){
     }
 
     private fun initUI() {
-
+        btnAddPatient = findViewById(R.id.btnAddPatient)
         spinnerGender = findViewById(R.id.spinnerGender)
         spinnerMaritalStatus = findViewById(R.id.spinnerMartialStatus)
         spinnerEducationLevel = findViewById(R.id.spinnerEducationLevel)
         spinnerGender.adapter = createAdapter(resources.getStringArray(R.array.gender_list).toList())
         spinnerMaritalStatus.adapter = createAdapter(resources.getStringArray(R.array.martial_status_list).toList())
         spinnerEducationLevel.adapter = createAdapter(resources.getStringArray(R.array.education_level_list).toList())
+
+        btnAddPatient.setOnClickListener {
+            val viewPatientIntent = Intent(this, ViewPatientActivity::class.java)
+            startActivity(viewPatientIntent)
+        }
     }
 
     private fun createAdapter(values: List<String>): SpinnerAdapter? {
-        var dataAdapter = ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, values!!.toMutableList())
+        val dataAdapter = ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, values!!.toMutableList())
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         return dataAdapter
     }
