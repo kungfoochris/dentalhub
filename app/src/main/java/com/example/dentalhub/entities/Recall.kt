b065a8aa10f5b1dd4be19254fe8fcd6cff1b820a
Paras Nath <opnchaudhary@gmail.com>
2019-07-15 15:51:57 +0545

    recall entity

---
+++ app/src/main/java/com/example/dentalhub/entities/Recall.kt
@@ -0,0 +1,19 @@
+package com.example.dentalhub.entities
+
+import android.os.Parcelable
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
+import io.objectbox.relation.ToOne
+import kotlinx.android.parcel.Parcelize
+
+@Entity
+@Parcelize
+class Recall: Parcelable {
+    @Id
+    var id: Long = 0
+    var date: String = ""
+    var geography: String = ""
+    var activity: String = ""
+    var patient: ToOne<Patient>? = null
+    var encounter: ToOne<Encounter>? = null
+}
