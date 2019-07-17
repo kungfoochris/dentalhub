018b177f17306e36f582e67e9b89cf009b66fad4
Paras Nath <opnchaudhary@gmail.com>
2019-07-17 13:29:45 +0545

    detect network availability

--- app/src/main/java/com/example/dentalhub/services/SyncService.kt
+++ app/src/main/java/com/example/dentalhub/services/SyncService.kt
@@ -2,19 +2,24 @@ package com.example.dentalhub.services
 
 import android.app.Service
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.IBinder
+import android.util.Log
 import com.example.dentalhub.DentalApp
 import com.example.dentalhub.ObjectBox
+import com.example.dentalhub.broadcastreceivers.NetworkStateReceiver
 import com.example.dentalhub.entities.Encounter
 import com.example.dentalhub.entities.Patient
 import io.objectbox.Box
 import kotlin.concurrent.fixedRateTimer
 
-class SyncService : Service() {
+class SyncService : Service(), NetworkStateReceiver.NetworkStateReceiverListener {
+
 
 
     private lateinit var patientsBox: Box<Patient>
     private lateinit var encountersBox: Box<Encounter>
+    private lateinit var networkStateReceiver: NetworkStateReceiver
 
     override fun onBind(p0: Intent?): IBinder? {
         return null
@@ -25,12 +30,38 @@ class SyncService : Service() {
         patientsBox = ObjectBox.boxStore.boxFor(Patient::class.java)
         encountersBox = ObjectBox.boxStore.boxFor(Encounter::class.java)
 
-        startSync()
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        networkStateReceiver = NetworkStateReceiver()
+        networkStateReceiver.addListener(this)
+        this.registerReceiver(networkStateReceiver, IntentFilter(android.net.ConnectivityManager.CONNECTIVITY_ACTION))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        networkStateReceiver.removeListener(this)
+        this.unregisterReceiver(networkStateReceiver)
     }
 
     private fun startSync() {
         displayNotification()
+    }
+    override fun networkAvailable() {
+       // startSync()
+        Log.d("SyncService", "networkAvailable()")
+    }
+
+    override fun networkUnavailable() {
+        //pauseSync()
+        Log.d("SyncService", "networkUnavailable()")
+    }
+
+    private fun pauseSync() {
+        // stop the sync
+        DentalApp.cancelNotification(1001)
     }
 
     private fun displayNotification() {
