804233cd9331c9477529db2265771d7ff7aff0e3
Paras Nath <paras@abhiyantrik.com>
2019-11-12 16:15:22 +0545

    worker to download user

--- app/src/main/java/com/abhiyantrik/dentalhub/workers/DownloadUsersWorker.kt
+++ app/src/main/java/com/abhiyantrik/dentalhub/workers/DownloadUsersWorker.kt
@@ -1,4 +1,65 @@
 package com.abhiyantrik.dentalhub.workers
 
-class DownloadUsersWorker {
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.abhiyantrik.dentalhub.Constants
+import com.abhiyantrik.dentalhub.DentalApp
+import com.abhiyantrik.dentalhub.ObjectBox
+import com.abhiyantrik.dentalhub.entities.Patient_
+import com.abhiyantrik.dentalhub.entities.User
+import com.abhiyantrik.dentalhub.entities.User_
+import com.abhiyantrik.dentalhub.interfaces.DjangoInterface
+import io.objectbox.Box
+import java.lang.Exception
+import com.abhiyantrik.dentalhub.models.User as UserModel
+
+class DownloadUsersWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+
+    private lateinit var usersBox: Box<User>
+
+    override fun doWork(): Result {
+        return try {
+            usersBox = ObjectBox.boxStore.boxFor(User::class.java)
+            downloadUsers()
+            return Result.success()
+        }catch (e: Exception){
+            Log.d("Exception", e.printStackTrace().toString())
+            Result.failure()
+        }
+    }
+
+    private fun downloadUsers() {
+
+        val token = DentalApp.readFromPreference(applicationContext, Constants.PREF_AUTH_TOKEN, "")
+        val panelService = DjangoInterface.create(applicationContext)
+        val call = panelService.listUsers("JWT $token")
+
+
+        val response = call.execute()
+        if (response.isSuccessful) {
+            when (response.code()) {
+                200 -> {
+                    val allUsers = response.body() as List<UserModel>
+                    for(user in allUsers){
+                        val existingUserCount = usersBox.query().equal(
+                            User_.remote_id,
+                            user.id
+                        ).build().count()
+                        if(existingUserCount<1){
+                            val userEntity = User()
+                            userEntity.remote_id = user.id
+                            userEntity.first_name = user.first_name
+                            userEntity.middle_name = user.middle_name
+                            userEntity.last_name = user.last_name
+                            usersBox.put(userEntity)
+                        }else{
+                            Log.d("DUser","User already download")
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
