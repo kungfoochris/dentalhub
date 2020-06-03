0f9ea3deb865dac49a9ed4cceebcf99bc45bb2a6
Your Name <you@example.com>
2020-06-03 20:43:35 +0545

    Flagencounter Activity added.

---
+++ app/src/main/java/com/abhiyantrik/dentalhub/ui/flagencounterview/FlagEncounterView.kt
@@ -0,0 +1,29 @@
+package com.abhiyantrik.dentalhub.ui.flagencounterview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.abhiyantrik.dentalhub.R
+import com.abhiyantrik.dentalhub.adapters.FlagAdapter
+import com.abhiyantrik.dentalhub.models.FlagEncounter
+import kotlinx.android.synthetic.main.activity_flag_encounter_view.*
+
+class FlagEncounterView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flag_encounter_view)
+
+        val flag1 = FlagEncounter("1", "Milan", "Checkup and screening", "modify", "Pending", "This is the testing of the flag")
+        val flag2 = FlagEncounter("2", "Paras Nath Chaudhary", "Relif of pain", "modify", "Expired", "This is the testing of the flag")
+        val flag3 = FlagEncounter("3", "Ghana Shyam", "Checkup and screening", "Delete", "Pending", "This is the testing of the flag")
+        val flag4 = FlagEncounter("4", "Prabin", "Checkup and screening", "modify", "Approved", "This is the testing of the flag")
+
+        val flagEcounterList = mutableListOf<FlagEncounter>(flag1, flag2, flag3, flag4)
+
+        val adapter = FlagAdapter(this, flagEcounterList)
+        rvFlagEncounter.adapter = adapter
+        rvFlagEncounter.layoutManager = LinearLayoutManager(this)
+    }
+}
