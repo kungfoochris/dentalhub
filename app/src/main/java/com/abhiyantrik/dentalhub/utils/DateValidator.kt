5dcb92749f8dbbc94c9df00801e4c67ca464eb33
Paras Nath Chaudhary <opnchaudhary@gmail.com>
2020-10-03 13:26:19 +0545

    updated deprecated codes

--- app/src/main/java/com/abhiyantrik/dentalhub/utils/DateValidator.kt
+++ app/src/main/java/com/abhiyantrik/dentalhub/utils/DateValidator.kt
@@ -17,10 +17,6 @@ class DateValidator {
                 val todayEnglish = nepaliCalender.getEnglishDate(year, month + 1, day)
                 val todayNepali = nepaliCalender.getNepaliDate(todayEnglish.year, todayEnglish.month + 1, todayEnglish.day)
 
-//                Log.d("DateValidator", "todayEnglish " + todayEnglish.year + todayEnglish.month + todayEnglish.day)
-//                Log.d("DateValidator", "todayNepali " + todayNepali.year + todayNepali.month + todayNepali.day)
-//                Log.d("DateValidator", "enteredbyUser " + year + month + day)
-//                format.parse(date)
                 if ((todayNepali.year == year) && (todayNepali.month == month) && (todayNepali.day == day)) {
                     status = true
                 }
